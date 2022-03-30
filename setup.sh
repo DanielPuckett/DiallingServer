144fa35c28507347a26e1712203305b787670f15
DanielPuckett <dpuckett@distributel.ca>
2022-03-30 10:38:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ chown www-data DiallingServer.sh runVSSP.sh xfileread.bash
 chmod u+rwx DiallingServer.sh runVSSP.sh xfileread.bash
 ln -s /opt/DiallingServer /var/www/DiallingServer
 ln -s /tmp ./tmp
+ln -s /opt/VSSP-Softphone/VSSP ./VSSP
