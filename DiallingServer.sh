683af43a9657c86a4c8e821385670f0e8cb6da6d
DanielPuckett <dpuckett@distributel.ca>
2022-03-31 09:34:08 -0400

    Update DiallingServer.sh
    Make the logger and phones on-demand auto start

--- DiallingServer.sh
+++ DiallingServer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Dialling Server is a 1 second idle loop looking for a non zero size did.ticket to process
 # Upon finding a ticket, it calls xfileread.bash to process the ticket.
@@ -6,14 +6,36 @@
 
 PHONES=6
 
+source /var/www/DiallingSite/processcontrol.src
+
 touch ./tmp/did.ticket
 
 while [ 1 ]; do
   sleep 1;
   if [ -s ./tmp/did.ticket ]; then
+    A="START"
+    P="CRAFTRECORDS"
+    processcontrol
+
+    L=$(($(($(cat ./tmp/did.ticket|wc -l) - 1 ))*2))
+
+    for i in $(echo "1 2 3 4 5 6"|colrm $L); do
+      P="PHONE${i}"
+      processcontrol
+    done
     /bin/echo -n "" > ./tmp/did.events.log
     /bin/echo "working" > ./tmp/did.ticket.working
     /bin/bash ../DiallingServer/xfileread.bash ./tmp/did.ticket $PHONES ./tmp/sip.in.
+
+    sleep 10
+    A="STOP"
+    for i in $(echo "1 2 3 4 5 6"|colrm $L); do
+      P="PHONE${i}"
+      processcontrol
+    done
+    P="CRAFTRECORDS"
+    processcontrol
+
     /bin/echo -n "" > ./tmp/did.ticket
     /bin/echo -n "" > ./tmp/did.ticket.working
     /bin/echo "CLOSED" >> ./tmp/did.events.log
