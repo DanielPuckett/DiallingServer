f485c6d2c711c76c2da83401afa4a6574c7eed7e
DanielPuckett <dpuckett@distributel.ca>
2022-03-31 15:24:20 -0400

    Update setup.sh
    Disposed of extra softlink to DiallingServer directory

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 cd /opt/DiallingServer
 chown www-data DiallingServer.sh runVSSP.sh xfileread.bash
 chmod u+rwx DiallingServer.sh runVSSP.sh xfileread.bash
-ln -s /opt/DiallingServer /var/www/DiallingServer
 ln -s /tmp ./tmp
 ln -s /opt/VSSP-Softphone/VSSP ./VSSP
