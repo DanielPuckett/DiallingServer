f8fcbee329ad451064ad4228f4a6b24e7b27225c
DanielPuckett <dpuckett@distributel.ca>
2022-04-04 10:06:22 -0400

    Update setup.sh
    Changed the names of DiallingServer.sh and runVSSP.sh to .bash.
    In Mac OSX we used the sh shell, but we use some functionality that is only in the Debian bash shell.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /opt/DiallingServer
-chown www-data DiallingServer.sh runVSSP.sh xfileread.bash
-chmod u+rwx DiallingServer.sh runVSSP.sh xfileread.bash
+chown www-data DiallingServer.bash runVSSP.bash xfileread.bash
+chmod u+rwx DiallingServer.bash runVSSP.bash xfileread.bash
 ln -s /tmp ./tmp
 ln -s /opt/VSSP-Softphone/VSSP ./VSSP
